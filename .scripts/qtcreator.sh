03fac4eaa462c511beaf87aa3a7b55e512347b27
oosman <oosman3@gmail.com>
2021-02-26 20:45:39 +0000

    update qtcreator.sh

--- .scripts/qtcreator.sh
+++ .scripts/qtcreator.sh
@@ -1,2 +1,2 @@
 #QT_DEBUG_PLUGINS=1
-LD_LIBRARY_PATH=$QTDIR/Tools/QtCreator/lib/Qt/lib/:/home/dev/$DOCKERUSER/.leila/libs/build/linux/bin/:$QTDIR/5.15.2/gcc_64/plugins/platforms $QTDIR/Tools/QtCreator/bin/qtcreator &
+LD_LIBRARY_PATH=$QTDIR/Tools/QtCreator/lib/Qt/lib/:/home/dev/$DOCKERUSER/.leila/build:$QTDIR/5.15.2/gcc_64/plugins/platforms $QTDIR/Tools/QtCreator/bin/qtcreator &
